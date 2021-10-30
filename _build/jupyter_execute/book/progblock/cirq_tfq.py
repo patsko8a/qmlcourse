ff34c421d5db3ec623d9df4caa6f357474412b55
SemyonSinchenko <SemyonSinchenko@users.noreply.github.com>
2021-10-30 18:27:48 +0000

    deploy: 97bda663429f449f694a1b35e3deae9486d72629

--- _build/jupyter_execute/book/progblock/cirq_tfq.py
+++ _build/jupyter_execute/book/progblock/cirq_tfq.py
@@ -17,6 +17,16 @@
 # 
 # ### Импорты и схема
 # 
+# ```{warning}
+# `Tensorflow Quantum` нельзя утсановить на `Windows`, поэтому те `Makefiles`, которые мы сделали для слушателей не содержат шаг установки `tensorflow-quantum`. Если ваша система `POSIX`-совместимая и вы хотите обязательно попробовать запустить примеры кода из этой лекции, то просто в корне данного репозитория наберите команду:
+# 
+# `poetry run pip install tensorflow-quantum==0.5.1`
+# 
+# Обладателям компьютеров на системе `Windows` мы можем лишь предложить использовать [`WSL2`](https://docs.microsoft.com/en-us/windows/wsl/about) или `Docker`. В целом эта лекция факультативная и нигде далее `TFQ` у нас не используется.
+# 
+# Больше информации можно найти в [разделе про установку](https://www.tensorflow.org/quantum/install) в официальной документации этой библиотеки.
+# ```
+# 
 # Для начала импортируем `cirq`.
 
 # In[1]:
@@ -68,19 +78,16 @@ sim = cirq.Simulator()
 # In[5]:
 
 
-print("5 сэмплов:")
-print(sim.sample(circuit, repetitions=5).mean())
-print("\n100 сэмплов:")
-print(sim.sample(circuit, repetitions=100).mean())
-print("\n1000 сэмплов:")
-print(sim.sample(circuit, repetitions=1000).mean())
+print(f"5 сэмплов: {sim.sample(circuit, repetitions=5).mean()}")
+print(f"\n100 сэмплов: {sim.sample(circuit, repetitions=100).mean()}")
+print(f"\n1000 сэмплов: {sim.sample(circuit, repetitions=1000).mean()}")
 
 
 # ```{note}
-# Метод `sim.sample` озвращает хорошо знакомый всем специалистам в области Data Science объект `pandas.DataFrame`. Для тех, кто слышит про такой впервые рекомендуем обратиться к вводным лекциям про `Python` и классическое машинное обучение.
+# Метод `sim.sample` возвращает хорошо знакомый всем специалистам в области Data Science объект `pandas.DataFrame`.
 # ```
 # 
-# Также у нас есть опция запустить схему через метод `run`. Может показаться, что это то же самое, но на самом деле в отличие от `sample` метод `run` возвращает результат в несколько ином виде, а еще он позволяет запускать программу на реальном квантовом компьютере `Goolge` или их новых квантовых симуляторах на TPU:
+# Также у нас есть опция запустить схему через метод `run`. Может показаться, что это то же самое, но на самом деле в отличие от `sample`, метод `run` возвращает результат в несколько ином виде; а еще он позволяет запускать программу на реальном квантовом компьютере `Goolge` или их новых квантовых симуляторах на TPU:
 
 # In[6]:
 
@@ -99,6 +106,8 @@ print(sim.run(circuit, repetitions=25))
 # In[7]:
 
 
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 import tensorflow_quantum as tfq
 
@@ -113,8 +122,7 @@ import tensorflow_quantum as tfq
 from sklearn.datasets import make_classification
 import numpy as np
 
-x, y = make_classification(n_samples=50, n_features=2, n_informative=2,
-                           random_state=42, n_redundant=0)
+x, y = make_classification(n_samples=50, n_features=2, n_informative=2, random_state=42, n_redundant=0)
 
 def normalize(x):
     x_min = x.min()
@@ -233,10 +241,7 @@ cirq_inputs = tf.keras.Input(shape=(), dtype=tf.dtypes.string)
 # In[16]:
 
 
-quantum_layer = tfq.layers.PQC(
-    trainable_circuit,
-    ops
-)(cirq_inputs)
+quantum_layer = tfq.layers.PQC(trainable_circuit, ops)(cirq_inputs)
 
 
 # - классический слой и выходной слой
@@ -287,7 +292,7 @@ plt.show()
 # In[20]:
 
 
-model.fit(x=x_input, y=y, epochs=200, verbose=0)
+get_ipython().run_cell_magic('time', '', 'model.fit(x=x_input, y=y, epochs=200, verbose=0)')
 
 
 # - визуализируем логи обучения
@@ -308,6 +313,7 @@ plt.show()
 
 preds_after_training = model(x_input).numpy()
 plt.figure(figsize=(6, 4))
+
 cb = plt.scatter(x[:, 0], x[:, 1], c=preds_after_training)
 plt.colorbar(cb)
 plt.show()
